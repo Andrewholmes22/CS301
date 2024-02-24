c1224816561d816b9d8945e5a6bf9eeff867eaad
NIKBOL42 <nikolbolas5@gmail.com>
2024-02-24 16:50:57 -0700

    start of hashing

--- week 7/ListSearch.py
+++ week 7/ListSearch.py
@@ -5,7 +5,6 @@ for x in range(200): #make list2
 # print(sortList1)
 # print(sortList2[:50])
 
-
 def search_sorted_list(sorted_list,item,low=0,high=None):
     if high == None:
         high = len(sorted_list)-1
@@ -20,10 +19,23 @@ def search_sorted_list(sorted_list,item,low=0,high=None):
         return search_sorted_list(sorted_list,item,mid+1,high)
     else:
         return search_sorted_list(sorted_list,item,low,mid-1)
-    
         
-print(search_sorted_list(sortList1,5))
-print(search_sorted_list(sortList1,3))
-print(search_sorted_list(sortList1,1))
-print(search_sorted_list(sortList1,9))
+# print(search_sorted_list(sortList1,5))
+# print(search_sorted_list(sortList1,3))
+# print(search_sorted_list(sortList1,1))
+# print(search_sorted_list(sortList1,9))
 
+inList = input("Input nums, comma seperated: ").split(',')
+hashList = [None]*(len(inList)*3)
+# print(inList)
+# print(len(hashList))
+def hasher_maker(inList,hashList):
+    l = len(hashList)
+    for num in  inList:
+        place = int(num) % l
+        while hashList[place] != None:
+            place += 1
+        hashList[place] = int(num)
+        
+hasher_maker(inList,hashList)
+print(hashList)
