7c9bbab5af5b614c4fa3f5aafed641a000aa65af
NIKBOL42 <nikolbolas5@gmail.com>
2024-03-03 20:37:16 -0700

    done sorting

--- week 7/List-Sorting.py
+++ week 7/List-Sorting.py
@@ -32,7 +32,7 @@ def Bubble_Sort(unsorted): #Bubble Sort
             break
 
 #Select Sort
-def SelectionSort(unsorted):  #O(n^2)
+def Selection_Sort(unsorted):  #O(n^2)
     for x in range(len(unsorted)):
         min_index = x
         for n in range(x + 1, len(unsorted)):
@@ -40,6 +40,7 @@ def SelectionSort(unsorted):  #O(n^2)
                 min_index = n
         unsorted[x], unsorted[min_index] = unsorted[min_index], unsorted[x]
     return unsorted
+
 def List_Merger(list1,list2):
     sort = []
     l1 = len(list1)
@@ -58,12 +59,37 @@ def List_Merger(list1,list2):
         else:
             inp = list2.pop(0)
         sort.append(inp)
-    print(sort)
+    return sort
 
 def Merge_Sort(unsorted): #Merge Sort
-    return
-
-def Bogo_Sort(): #BOGO Sort
+    if len(unsorted) > 1:
+        mid = len(unsorted)//2
+        l1 = unsorted[:mid]
+        l2 = unsorted[mid:]
+        Merge_Sort(l1)
+        Merge_Sort(l2)
+        i=0
+        j=0
+        k=0
+        while i<len(l1) and j<len(l2):
+            if l1[i] < l2[j]:
+                unsorted[k] = l1[i]
+                i+=1
+            else:
+                unsorted[k] = l2[j]
+                j+=1
+            k+=1
+        while i < len(l1):
+            unsorted[k] = l1[i]
+            i+=1
+            k+=1
+        while j < len(l2):
+            unsorted[k] = l2[j]
+            j+=1
+            k += 1
+        return unsorted
+    
+def Bogo_Sort(): #BOGO Sort O(Very High)
     unsorted = listMaker(10)
     sort = sorted(unsorted)
     count = 0
@@ -104,6 +130,14 @@ total = end-start
 string = "Bubble sort time: {time:.2f} seconds"
 print(string.format(time=total))
 
-list1 = [1,3,5,7,9]
-list2 = [2,4,6,8,10]
-List_Merger(list1,list2)
+# list1 = [1,3,5,7,9]
+# list2 = [2,4,6,8,10]
+# List_Merger(list1,list2)
+
+unsorted = listMaker(1000000)
+start = time.time()
+Merge_Sort(unsorted)
+end = time.time()
+total = end-start
+string = "Merge sort time: {time:.2f} seconds"
+print(string.format(time=total))
