e7ac0c345d6ec8fdd8a3f33ebccbd4bad0e0be05
Andrewholmes22 <111909041+Andrewholmes22@users.noreply.github.com>
2024-03-17 20:45:15 -0600

    insert file still not working but everything else should be good

--- week 9/DirectoryTree.py
+++ week 9/DirectoryTree.py
@@ -6,7 +6,7 @@ class DirectoryTree():
     def __init__(self,root='/'):
         self.root = root
         self.tree = [root]
-    def insertPath(self,path):
+    def insert_file(self,path):
         pathLi = path.split('/')
         pathLi.pop(0)
         while len(pathLi)>1:
@@ -18,151 +18,51 @@ class DirectoryTree():
                 pathLi.pop(0)
                 pathLi.pop(1)
             
-    def printTree(self):
-        print(self.tree)
+    def delete_file(self, path):
+        directories = path.split('/')[1:]
+        current_node = self.root
+
+        for directory in directories[:-1]:
+            found = False
+            for child in current_node.children:
+                if child.data == directory:
+                    current_node = child
+                    found = True
+                    break
+            if not found:
+                raise IndexError('File Not Found')
+
+        file_name = directories[-1]
+        found = False
+        for child in current_node.children:
+            if child.data == file_name:
+                current_node.children.remove(child)
+                found = True
+                break
+
+        if not found:
+            raise IndexError('File Not Found')
+
+    def move_file(self, source_path, destination_path):
+        try:
+            self.insert_file(destination_path)
+            self.delete_file(source_path)
+        except IndexError as e:
+            raise IndexError(str(e))
+
+    def _print_tree_recursive(self, node, depth):
+        if isinstance(node, Node):
+            print('  ' * depth + node.data)
+            for child in node.children:
+                self._print_tree_recursive(child, depth + 1)
+        else:
+            print('  ' * depth + node)
+
+    def print_tree(self):
+        self._print_tree_recursive(self.root, 0)
+
 
 newTree = DirectoryTree()
-newTree.printTree()
-newTree.insertPath('/usr/log/log1')
-newTree.printTree()
-newTree.insertPath('/usr/log/log2')
-newTree.printTree()
-
-class Node():
-    def __init__(self, item):
-        self.item = item
-        self.next = None
-
-class linked_list():
-    def __init__(self):
-        self.head = None
-
-    def add(self, item):
-        newNode = Node(item)
-        if self.head is None:
-            self.head = newNode
-            
-        else:
-            newNode.next = self.head
-            self.head = newNode
-
-    def remove(self, item):
-        if self.head is None:
-            raise KeyError
-        elif self.head.item == item:
-            self.head = self.head.next
-            return
-        else:
-            currentNode = self.head
-            while currentNode:
-                if currentNode.next is None:
-                    raise KeyError
-                elif currentNode.next.item == item:
-                    currentNode.next = currentNode.next.next
-                    return
-
-                currentNode = currentNode.next
-
-    def search(self, item):
-        currentNode = self.head
-        while currentNode:
-            if currentNode.item == item:
-                return True
-            currentNode = currentNode.next
-        return False
-    
-    def isEmpty(self):
-        if self.head is None:
-            return True
-        return False
-    
-    def size(self):
-        currentNode = self.head
-        counter = 0
-        if self.head is None:
-            return 0
-        else:
-            while currentNode:
-                counter += 1
-                currentNode = currentNode.next
-            return counter
-
-    def printAll(self):
-        currentNode = self.head
-        while currentNode:
-            print(currentNode.item)
-            currentNode = currentNode.next
-
-    def append(self, item):
-        newNode = Node(item)
-        currentNode = self.head
-        if self.head is None:
-            self.add(item)
-        else:
-            while currentNode:
-                if currentNode.next is None:
-                    currentNode.next = newNode
-                    return
-                currentNode = currentNode.next
-
-    def index(self, item):
-        currentNode = self.head
-        counter = 0
-        if self.head is None:
-            raise KeyError
-        else:
-            while currentNode:
-                if currentNode.item == item:
-                    return counter
-                currentNode = currentNode.next
-                counter += 1
-            raise KeyError
-        
-    def insert(self, position, item):
-        currentNode = self.head
-        newNode = Node(item)
-        counter = 1
-        if self.size() <= position:
-            raise ValueError
-        elif position == 0:
-            self.add(item)
-        else:
-            while currentNode:
-                if counter == position:
-                    newNode.next = currentNode.next
-                    currentNode.next = newNode
-                    return
-                currentNode = currentNode.next
-                counter += 1
-
-    def pop(self, pos = None):
-        currentNode = self.head
-        counter = 1
-        if self.head is None:
-            raise ValueError
-        
-        elif pos == None or pos == self.size()-1:
-            while currentNode:
-                if currentNode.next.next is None:
-                    returnItem = currentNode.next
-                    currentNode.next = None
-                    return returnItem
-
-                currentNode = currentNode.next
-
-        elif self.size() <= pos:
-            raise ValueError
-
-        elif pos == 0:
-            returnItem = self.head
-            self.head = currentNode.next
-            return returnItem
-
-        else:
-            while currentNode:
-                if counter == pos:
-                    returnItem = currentNode.next
-                    currentNode.next = currentNode.next.next
-                    return returnItem
-                counter += 1
-                currentNode = currentNode.next
+newTree.insert_file('/usr/log/log1')
+newTree.insert_file('/usr/log/log2')
+newTree.print_tree()
