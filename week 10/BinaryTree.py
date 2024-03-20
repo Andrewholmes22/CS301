5adba097da478c5b2c60f5d07ed99436810f2d81
NIKBOL42 <nikolbolas5@gmail.com>
2024-03-20 15:22:39 -0600

    progress!

--- week 10/BinaryTree.py
+++ week 10/BinaryTree.py
@@ -1,8 +1,47 @@
 class Node:
-    def __init__(self, item):
-        self.item = item
-        self.children = []
+    def __init__(self, root):
+        self.item = root
+        self.LeftChild = -1
+        self.RightChild = -1
 
 class DirectoryTree:
-    def __init__(self, root='/'):
+    def __init__(self,root):
         self.root = Node(root)
+        
+    def insert(self,item):
+        current_node = self.root
+        Found = False
+        while not Found:
+            if item <= current_node.item:
+                if current_node.LeftChild == -1:
+                    current_node.LeftChild = Node(item)
+                    Found = True
+                else:
+                    current_node = current_node.LeftChild
+            elif item > current_node.item:
+                if current_node.RightChild == -1:
+                    current_node.RightChild = Node(item)
+                    Found = True
+                else:
+                    current_node = current_node.RightChild
+    def search(self,item):
+        curr_node = self.root
+        found = False
+        while not found:
+            if item == curr_node.item:
+                found = True
+                return True
+            elif item <= curr_node.item:
+                curr_node = curr_node.LeftChild
+            elif item > curr_node.item:
+                curr_node = curr_node.RightChild
+                
+biTree = DirectoryTree(5)
+biTree.insert(4)
+biTree.insert(6)
+biTree.insert(3)
+biTree.insert(7)
+print(biTree.search(5))
+print(biTree.search(3))
+print(biTree.search(7))
+print(biTree.search(9))
