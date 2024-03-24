7960820a16144d262a766a7926a8c93fe80c8f49
Andrewholmes22 <111909041+Andrewholmes22@users.noreply.github.com>
2024-03-23 18:38:21 -0600

    reverse sorted list

--- scratch paper/bsttest.py
+++ scratch paper/bsttest.py
@@ -1,14 +1,14 @@
 class Node:
-    def __init__(self, root = -1):
+    def __init__(self, root=-1):
         self.item = root
         self.LeftChild = -1
         self.RightChild = -1
 
 class DirectoryTree:
-    def __init__(self,root):
+    def __init__(self, root):
         self.root = Node(root)
         
-    def insert(self,item):
+    def insert(self, item):
         current_node = self.root
         Found = False
         while not Found:
@@ -26,7 +26,8 @@ class DirectoryTree:
                     break
                 else:
                     current_node = current_node.RightChild
-    def search(self,item):
+                    
+    def search(self, item):
         curr_node = self.root
         found = False
         while not found:
@@ -39,34 +40,38 @@ class DirectoryTree:
                 curr_node = curr_node.LeftChild
             elif item > curr_node.item:
                 curr_node = curr_node.RightChild
-    def sorted_list(self,start = None):
-        if start == None:
-            start = self.root
+                
+    def sorted_list(self, start=None):
         sortList = []
+        if start is None:
+            start = self.root
         if start.LeftChild != -1:
-            self.sorted_list(start = start.LeftChild)
-        elif start.LeftChild == -1:
-            sortList.append(start.item)
+            sortList += self.sorted_list(start.LeftChild)
+        sortList.append(start.item)
+        if start.RightChild != -1:
+            sortList += self.sorted_list(start.RightChild)
         return sortList
     
-    def reverse_sorted_list(self,start = None):
-        if start == None:
-            start = self.root
+    def reverse_sorted_list(self, start=None):
         sortList = []
+        if start is None:
+            start = self.root
         if start.RightChild != -1:
-            self.reverse_sorted_list(start = start.RightChild)
-        elif start.RightChild == -1:
-            sortList.append(start.item)
+            sortList += self.reverse_sorted_list(start.RightChild)
+        sortList.append(start.item)
+        if start.LeftChild != -1:
+            sortList += self.reverse_sorted_list(start.LeftChild)
         return sortList
-        
-                
+
+# Example usage:
 biTree = DirectoryTree(5)
 biTree.insert(4)
 biTree.insert(6)
 biTree.insert(3)
 biTree.insert(7)
-print(biTree.search(5))#return True
-print(biTree.search(9))#return False
-print(biTree.search(7))#return True
-print(biTree.search(3))#return True
-print(biTree.sorted_list())
+print(biTree.search(5))  # Output: True
+print(biTree.search(9))  # Output: False
+print(biTree.search(7))  # Output: True
+print(biTree.search(3))  # Output: True
+print(biTree.sorted_list())  # Output: [3, 4, 5, 6, 7]
+print(biTree.reverse_sorted_list())
