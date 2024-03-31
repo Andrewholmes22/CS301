8b69b91bb7960b1dcf80b7f413723d10e862b434
Andrewholmes22 <111909041+Andrewholmes22@users.noreply.github.com>
2024-03-31 13:10:34 -0600

    done

--- week 11/balancedTree.py
+++ week 11/balancedTree.py
@@ -1,8 +1,8 @@
 class Node:
     def __init__(self, root = -1):
-        self.item =root
-        self.left = -1
-        self.right = -1
+        self.item = root
+        self.left = None
+        self.right = None
         self.height = 1
 
 class AVLTree:
@@ -74,58 +74,54 @@ class AVLTree:
             return self.getHeight(root.left) - self.getHeight(root.right)
 
     
-    def search(self,item):
+    def search(self, item):
         curr_node = self.root
-        found = False
-        while not found:
+        while curr_node is not None:
             if item == curr_node.item:
-                found = True
                 return True
-            elif curr_node.left == -1 and curr_node.right == -1:
-                return False
-            elif item <= curr_node.item:
-                curr_node = curr_node.Left
-            elif item > curr_node.item:
-                curr_node = curr_node.Right
-    #best case is O(1), when the item being searched for is found at the root 
-    #worst case is O(n), when the item being searched is not in the tree or is at the last level of the tree
+            elif item < curr_node.item:
+                curr_node = curr_node.left
+            else:
+                curr_node = curr_node.right
+        return False
     
     def sorted_list(self, start=None):
         sortList = []
         if start is None:
             start = self.root
-        if start.left != -1:
-            sortList += self.sorted_list(start.lefteft)
+
+        if start.left is not None:
+            sortList += self.sorted_list(start.left)
+            
         sortList.append(start.item)
-        if start.right != -1:
+        
+        if start.right is not None:
             sortList += self.sorted_list(start.right)
+
         return sortList
-    #best case is O(n), when the tree is balanced, the algorithm would traverse each node once, adding each item to the sorted list.
-    #worst case is O(n), when the tree is unbalanced each node only having one child needs to traverse all 'n' nodes in the tree recursively
-    
-    def reverse_sorted_list(self,start = None):
-        if start == None:
+ 
+    def reverse_sorted_list(self, start=None):
+        if start is None:
             start = self.root
         sortList = []
-        if start.right != -1:
-            self.reverse_sorted_list(start = start.right)
-        elif start.right == -1:
-            sortList.append(start.item)
+        if start.right is not None:
+            sortList += self.reverse_sorted_list(start.right)
+        sortList.append(start.item)
+        if start.left is not None:
+            sortList += self.reverse_sorted_list(start.left)
         return sortList
-    # best case O(n) when the tree is perfectly balanced, and every node has both a left and right child
-    # worst case is O(n), when the tree is unbalanced each node only having one child needs to traverse all 'n' nodes in the tree recursively         
-Tree = AVLTree(0)
-root = None
+    
+Tree = AVLTree(0)  
+Tree.root = Tree.insert(Tree.root, 1)
+Tree.root = Tree.insert(Tree.root, 5)
+Tree.root = Tree.insert(Tree.root, 7)
+Tree.root = Tree.insert(Tree.root, 3)
+Tree.root = Tree.insert(Tree.root, 8)
+Tree.root = Tree.insert(Tree.root, 10)
 
-Tree.insert(root, 1)
-Tree.insert(root, 2)
-Tree.insert(root, 3)
-Tree.insert(root, 4)
-Tree.insert(root, 5)
-Tree.insert(root, 6)
-
-print(Tree.search(5))#return True
-print(Tree.search(9))#return False
-print(Tree.search(7))#return True
-print(Tree.search(3))#return True
+print(Tree.search(5))  # return True
+print(Tree.search(9))  # return False
+print(Tree.search(7))  # return False
+print(Tree.search(3))  # return True
 print(Tree.sorted_list())
+print(Tree.reverse_sorted_list())
