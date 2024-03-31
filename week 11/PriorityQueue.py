2fc03ce8e1fdced5496087023747a884e7edeb4d
NIKBOL42 <nikolbolas5@gmail.com>
2024-03-31 13:01:40 -0600

    now its a heap lol

--- week 11/PriorityQueue.py
+++ week 11/PriorityQueue.py
@@ -1,36 +1,64 @@
 class PriorityQueue:
     def __init__(self):
-        self.queue = []
-    def sortFunc(self,tup):
-        tup = tup
-        return tup[1]
-    def sortQueue(self): 
-        self.queue.sort(reverse = True,key=self.sortFunc)
-        #python sort is very efficient, O(n log n)
-    def insert(self,item,priority): 
-        tempTuple = (item,priority)
-        self.queue.append(tempTuple)
-        self.sortQueue()
-        #this is very fast, O(1), as it inserts the item at the end
-    def pop(self): 
-        out = self.queue.pop(0)
-        self.sortQueue()
-        return out
-        #this is probably the slowest, O(n), as it pops from the front
-        #but depending on need it could swap speeds with insert
-    def returnQueue(self):
-        outQueue = self.queue
-        return outQueue
-        #O(n) as it copies and sends out a different list.
+        self.heap = []
 
-newPriQueue = PriorityQueue()
-newPriQueue.insert(1,4)
-newPriQueue.insert(2,3)
-newPriQueue.insert(3,2)
-newPriQueue.insert(4,1)
-newPriQueue.insert(5,1)
-print(newPriQueue.returnQueue())
-print(newPriQueue.pop())
-print(newPriQueue.returnQueue())
-        
-        
+    def parent(self, i):
+        return (i - 1) // 2
+
+    def left_child(self, i):
+        return 2 * i + 1
+
+    def right_child(self, i):
+        return 2 * i + 2
+
+    def swap(self, i, j):
+        self.heap[i], self.heap[j] = self.heap[j], self.heap[i]
+
+    def insert(self, item, priority): #O(1) as it appends to the end, then sorts
+        self.heap.append((item, priority))
+        self.heapify_up(len(self.heap) - 1)
+
+    def heapify_up(self, i): #O(log n), sorting is done quickly by heap rules, not fully sorted rules
+        while i > 0 and self.heap[i][1] > self.heap[self.parent(i)][1]:
+            self.swap(i, self.parent(i))
+            i = self.parent(i)
+
+    def heapify_down(self, i): #O(log n), same as heaping up, this is quite quick
+        max_index = i
+        left = self.left_child(i)
+        right = self.right_child(i)
+
+        if left < len(self.heap) and self.heap[left][1] > self.heap[max_index][1]:
+            max_index = left
+
+        if right < len(self.heap) and self.heap[right][1] > self.heap[max_index][1]:
+            max_index = right
+
+        if i != max_index:
+            self.swap(i, max_index)
+            self.heapify_down(max_index)
+
+    def pop(self): #pop is fast, calling from the end means it doesn't need to shift anything. O(log n) from sorting
+        if len(self.heap) == 0:
+            return None
+
+        max_element = self.heap[0]
+        last_element = self.heap.pop()
+        if len(self.heap) > 0:
+            self.heap[0] = last_element
+            self.heapify_down(0)
+        return max_element
+    
+    def returnQueue(self):
+        outQueue = self.heap
+        return outQueue
+
+pq = PriorityQueue()
+pq.insert("task1", 5)
+pq.insert("task2", 3)
+pq.insert("task3", 8)
+pq.insert("task4", 2)
+print(pq.returnQueue())
+pq.pop()
+pq.pop()
+print(pq.returnQueue())
