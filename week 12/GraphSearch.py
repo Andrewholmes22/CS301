2065cbb0f5a003bdce9879858a772851fd7a1a95
NIKBOL42 <nikolbolas5@gmail.com>
2024-04-12 15:23:47 -0600

    some progress on matrix

--- week 12/GraphSearch.py
+++ week 12/GraphSearch.py
@@ -4,11 +4,32 @@ class AdjacencyMatrix:
         self.vertices = []
     def readGraph(self,filepath):
         fi = open(filepath)
-        num_verts,num_edges = fi.readline()
-        print(num_verts,num_edges)
-        
+        val = fi.readline().split()
+        num_Verts = int(val[0])
+        num_Edges = int(val[1])
+        self.vertices = fi.readline().strip().split(',')
+        self.graph = [['0' for _ in range(num_Verts+1)]for _ in range(num_Verts+1)]
+        for i in range(1,len(self.vertices)+1):
+            self.graph[0][i] = self.vertices[i-1]
+            self.graph[i][0] = self.vertices[i-1]
+            
+        for line in fi:
+            numbLine = line.strip().split(' ')
+            self.graph[self.vertices.index(numbLine[0])+1][self.vertices.index(numbLine[1])+1] = '1'
+            self.graph[self.vertices.index(numbLine[1])+1][self.vertices.index(numbLine[0])+1] = '1'
+        for line in self.graph:
+            print(line)
+        return True
     def addVertex(self,vertex):
-        pass
+        if vertex not in self.vertices:
+            self.vertices.append(vertex)
+            self.graph.append(vertex)
+            for line in self.graph:
+                if line == 1:
+                    self.graph.append(vertex)
+                else:
+                    self.graph.append('0')
+        return True
     def addEdge(self,edge):
         pass
     def deleteVertex(self,vertex):
@@ -37,4 +58,5 @@ class AdjacencyList:
 fpath = input("File Path: ")
 
 newMat = AdjacencyMatrix()
-newMat.readGraph(fpath)
+print(newMat.readGraph(fpath))
+#print(newMat.addVertex('u'))
